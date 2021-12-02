66f79a44a04d6da19c9a54dce6ae0e58f40ee6c0
mobob <bobdurie@gmail.com>
2021-12-01 20:50:51 -0500

    fun: added launch config to debug ths ts

--- src/2021/day1.ts
+++ src/2021/day1.ts
@@ -1,19 +1,19 @@
 var fs = require("fs");
-var array = fs.readFileSync("day1.txt").toString().trim().split("\n");
+var array = fs.readFileSync("src/2021/day1.txt").toString().trim().split("\n");
 console.log(`parsed: ${array.length} elements, first one is ${array[0]}`);
-var lineLengths : {[length: number] : number} = {};
-array.forEach((element : string) => {
+var lineLengths: { [length: number]: number } = {};
+array.forEach((element: string) => {
   const len = element.trim().length;
   lineLengths[len] = (lineLengths[len] ?? 0) + 1;
 });
 
 var increasingCount = 0;
-for(let key in lineLengths) {
+for (let key in lineLengths) {
   //console.log(`number of length ${key}: ${lineLengths[key]}`);
 }
 
 // skip the first
-for(let i = 1 ; i < array.length ; i++) {
+for (let i = 1; i < array.length; i++) {
   const last = parseInt(array[i - 1]);
   const cur = parseInt(array[i]);
   increasingCount += cur > last ? 1 : 0;
@@ -24,13 +24,13 @@ console.log(increasingCount);
 // part 2 - only chunks of 3 - so start there
 increasingCount = 0;
 const sumamt = 3;
-for(let i = sumamt ; i < array.length ; i++) {
+for (let i = sumamt; i < array.length; i++) {
   var lastSum = 0;
-  for(let lasti = i - sumamt ; lasti < i ; lasti++) {
+  for (let lasti = i - sumamt; lasti < i; lasti++) {
     lastSum += parseInt(array[lasti]);
   }
   var curSum = 0;
-  for(let lasti = i - sumamt + 1; lasti <= i ; lasti++) {
+  for (let lasti = i - sumamt + 1; lasti <= i; lasti++) {
     curSum += parseInt(array[lasti]);
   }
 
