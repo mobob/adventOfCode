b75a4bbf24b9c85a482f0f08e048cb4ade0bed1e
mobob <bobdurie@gmail.com>
2021-12-26 18:44:02 -0500

    fun: day 21 part 2 checkpoint 3...  ugg pairs of scores

--- src/2021/day21part2.ts
+++ src/2021/day21part2.ts
@@ -86,6 +86,7 @@ class DiracDice {
     [1, 2, 3].forEach((r1) => {
       [1, 2, 3].forEach((r2) => {
         [1, 2, 3].forEach((r3) => {
+          console.log(`${r1} ${r2} ${r3}`);
           result.push(r1 + r2 + r3);
         });
       });
@@ -124,29 +125,6 @@ class DiracDice {
     startingPosToNewPositionPossibilities[i] = counts;
   }
 
-  // we also need to build up the unique dice rolls (the incremental scores) that can be used
-  // to get to a given spot. for instance, if we end up at spot 5, there is 1 roll of 3 (from 2),
-  // 3 rolls of 4 (from 1), 6 rolls of 5 (from 10), 7 rolls of 6 (from 9), ...
-  // so = drctltrp[5] = [0, 0, 0, 1, 3, 6, 7, 6, 3, 1] (array size of 10, max ind is 9 as the max die roll)
-  const diceRollCountsThatLedToResultantPosition = [
-    0, 0, 0, 1, 3, 6, 7, 6, 3, 1,
-  ];
-  // new Array<number[]>(11);
-  // for (
-  //   var resultantPosition = 0;
-  //   resultantPosition <= 10;
-  //   resultantPosition++
-  // ) {
-  //   diceRollCountThatLedToResultantPosition[resultantPosition] =
-  //     new Array<number>(10).fill(0);
-  //   if (resultantPosition < 3) {
-  //     continue;
-  //   }
-  //   startingPosToNewPositionPossibilities[i].forEach(
-  //     (newPositionPossibliites, newPosition) => {}
-  //   );
-  // }
-
   // the above shows it just cascades around, if pos starts at 1, adding starting at 4 1,3,6,7,6,3,1 up to 10.
 
   // hence after every turn there are 27 individual new scores, but actually only 7 unique values
@@ -169,144 +147,125 @@ class DiracDice {
       });
   });
 
-  // scores are all zero, but we init our two players both at score of 0
-  const scoreCounts = [
-    new Array<number>(21).fill(0),
-    new Array<number>(21).fill(0),
-  ];
-  scoreCounts[0][0] = 1;
-  scoreCounts[1][0] = 1;
+  // we acutally need a count, starting with each score, to a count of each position
+  // of that score!
+  const scoreMapPerPlayer = new Array<number[][]>(2);
+  for (
+    var playerIndex = 0;
+    playerIndex < scoreMapPerPlayer.length;
+    playerIndex++
+  ) {
+    const scoreMap = new Array<number[]>(21); // we need 0 -> 20
+    for (var score = 0; score < scoreMap.length; score++) {
+      scoreMap[score] = new Array<number>(11).fill(0);
+    }
+    scoreMapPerPlayer[playerIndex] = scoreMap;
+  }
 
-  var noWinnersThisTurn = true;
+  // set our inital positions, score 0, and one game each
+  startingPositions.forEach((pos, playerIndex) => {
+    scoreMapPerPlayer[playerIndex][0][pos] = 1;
+  });
+
   var winningCounts = [0, 0];
   var currentTurn = 0;
   var turnCount = 0;
   while (
-    // don't test if no winners this turn
-    noWinnersThisTurn ||
     // ensure there is something in the score array by counting it all up, and continue
     // only if both of them have a score
-    (scoreCounts[0].reduce((prev, cur) => {
-      return prev + cur;
-    }, 0) > 0 &&
-      scoreCounts[1].reduce((prev, cur) => {
-        return prev + cur;
-      }, 0) > 0)
+    scoreMapPerPlayer.reduce((prev: number, scores) => {
+      return (
+        prev +
+        scores.reduce((prev: number, positions) => {
+          return (
+            prev +
+            positions.reduce((prev: number, positionCount) => {
+              return prev + positionCount;
+            }, 0)
+          );
+        }, 0)
+      );
+    }, 0) > 0
   ) {
     // take our turn
 
-    // determine what the next turn looks like and how many we have in each position
-    // we want a map of currentPosition -> (newPosition -> resultantCountFromOldToNewPosition)
-    // var positionCountIncrements = new Array<Array<number>>(11).fill([]);
-    var newPositionCounts = new Array<number>(11).fill(0);
-    const newScoreCounts = new Array<number>(21).fill(0);
+    // BUG - it seems we're advancing too fast
+    // we should occupy every slot until the end...  yet it seems like we're advancing by 2
+    // every turn...
 
-    positionCounts[currentTurn].forEach((numInPosition, position) => {
-      if (position === 0) return;
-      // positionCountIncrements[position] = new Array<number>(11).fill(0);
-      startingPosToNewPositionPossibilities[position].forEach(
-        (newPositionCount, newPosition) => {
-          if (newPosition === 0) return;
-          // positionCountIncrements[position][newPosition] =
-          //   newPositionCount * numInPosition;
+    // init a new score map we'll populate as we go, and will swap at the end
+    const newScoreMap = new Array<number[]>(21); // we need 0 -> 20
+    for (var score = 0; score < newScoreMap.length; score++) {
+      newScoreMap[score] = new Array<number>(11).fill(0);
+    }
 
-          const newPositionCountIncrement = newPositionCount * numInPosition;
-          newPositionCounts[newPosition] += newPositionCountIncrement;
+    scoreMapPerPlayer[currentTurn].forEach((positionList, score) => {
+      positionList.forEach((countInCurrentPosition, currentPosition) => {
+        if (currentPosition === 0) return;
+        if (countInCurrentPosition === 0) return;
 
-          if (newPositionCountIncrement === 0) return;
+        // go through all the possible new positions for this given position
+        startingPosToNewPositionPossibilities[currentPosition].forEach(
+          (newPositionCount, newPosition) => {
+            if (newPosition === 0) return;
 
-          // now we do scores
-          for (var curScore = 20; curScore >= 0; curScore--) {
-            // if we didn't have any at the original score, we can skip this
-            if (scoreCounts[currentTurn][curScore] === 0) continue;
+            const newPositionCountIncrement =
+              newPositionCount * countInCurrentPosition;
 
-            const newScore = curScore + newPosition;
+            if (newPositionCountIncrement === 0) return;
+
+            const newScore = score + newPosition;
             if (newScore >= 21) {
-              // we have a winner! - not sure this is right?
+              // we have a winner! in this many universes this player just won
               winningCounts[currentTurn] += newPositionCountIncrement;
-              noWinnersThisTurn = false;
             } else {
-              newScoreCounts[newScore] += newPositionCountIncrement;
+              // if (newScoreMap[newScore][newPosition] !== 0) {
+              //   console.log(
+              //     `not 0 ${newScoreMap[newScore][newPosition]} + ${newPositionCountIncrement}`
+              //   );
+              // }
+              // otherwise we should add all the resultant new scores in this given new position for next round
+              newScoreMap[newScore][newPosition] += newPositionCountIncrement;
             }
-
-            // // in both cases, subtract from the position as we've consumed it
-            // newScoreCounts[curScore] -= numInPosition;
-
-            // // SANITY
-            // if (newScoreCounts[curScore] < 0) {
-            //   throw `took off too much score counts`;
-            // }
           }
-        }
+        );
+      });
+    });
+
+    scoreMapPerPlayer[currentTurn] = newScoreMap;
+
+    // NOW - assuming this isn't the first turn, we need to multiple our score map by 27
+    // to account for the other players 27
+    //if (turnCount !== 0) {
+    scoreMapPerPlayer[currentTurn].forEach((positionCounts, score) => {
+      positionCounts.forEach((count, index) => {
+        positionCounts[index] *= 9;
+      });
+    });
+    //}
+
+    turnCount++;
+    // console.log(
+    //   `t:${turnCount} player ${currentTurn}: score counts: ${scoreMapPerPlayer[currentTurn]}`
+    // );
+    scoreMapPerPlayer[currentTurn].forEach((positionCounts, score) => {
+      console.log(
+        `t:${turnCount} player ${currentTurn}: score ${score} counts at position: ${positionCounts}`
       );
     });
 
-    // we can re-assign the position counts, just gotta map it together
-    positionCounts[currentTurn] = newPositionCounts;
-    scoreCounts[currentTurn] = newScoreCounts;
-
-    turnCount++;
-    console.log(
-      `t:${turnCount} player ${currentTurn}: score counts: ${scoreCounts[currentTurn]}, pos: ${positionCounts[currentTurn]}`
+    const total = scoreMapPerPlayer[currentTurn].reduce(
+      (prev: number, positions) => {
+        return (
+          prev +
+          positions.reduce((prev: number, positionCount) => {
+            return prev + positionCount;
+          }, 0)
+        );
+      },
+      0
     );
-
-    console.log(
-      `num positions: ${positionCounts[currentTurn].reduce((prev, cur) => {
-        return prev + cur;
-      })}`
-    );
-    console.log(
-      `num scores: ${scoreCounts[currentTurn].reduce((prev, cur) => {
-        return prev + cur;
-      })}`
-    );
-
-    // positionCounts[currentTurn].forEach((numInPosition, position) => {
-    //   if (position === 0) return;
-    //   startingPosToNewPositionPossibilities[position].forEach(
-    //     (newPositionCount, newPosition) => {
-    //       if (newPosition === 0) return;
-    //       positionCountIncrements[newPosition] +=
-    //         newPositionCount * numInPosition;
-    //     }
-    //   );
-    // });
-
-    // increment our current positions
-    // TODO - NO - we just replace the array, as all those positions will advance
-    //positionCounts[currentTurn] = positionCountIncrements;
-    // positionCountIncrements.forEach((inc, position) => {
-    //   positionCounts[currentTurn][position] += inc;
-    // });
-
-    // do up our scores - we can start at the max and work our way down to avoid clobbering values
-    // for (var curScore = 20; curScore >= 0; curScore--) {
-    //   positionCountIncrements.forEach((inc, afterMovePosition) => {
-    //     //positionCounts[currentTurn].forEach((currentPositionCount, currentPosition) => {
-    //     if (afterMovePosition === 0) return;
-
-    //     // the after move position is the incremental score we need to add
-
-    //     // and inc is the number from the multiplier from the original position???
-
-    //     // do the scores - go through each die count increment
-
-    //     // if the ROLL (ie score increment) + current scores will move us to >=21, then we need
-    //     // to flag as won the number
-    //     if (afterMovePosition + curScore >= 21) {
-    //       // a winner! the dice roll count, * the number that were in this position are now winners
-    //       winningCounts[currentTurn] += inc;
-    //       noWinnersThisTurn = false;
-    //     } else {
-    //       scoreCounts[currentTurn][afterMovePosition + curScore] += inc; //scoreCounts[currentTurn][afterMovePosition] * inc;
-    //     }
-
-    //     // in both cases, subtract from the position as we've consumed it
-    //     scoreCounts[currentTurn][afterMovePosition] -= inc;
-
-    //     ///// leaving off here...  need a clear head :)
-    //   });
-    // }
+    console.log(`total: ${total}, winningTotals: ${winningCounts}`);
 
     currentTurn++;
     currentTurn %= 2;
